5dad95cff7af2171fc35bb6501298411df001bdc
Zach <zakhar.volchak@gmail.com>
2017-06-10 16:40:21 -0600

    init commit
    latest code

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
+
+
+$(".modal-wide").on("show.bs.modal", function() {
+  var height = $(window).height() + 777;
+  $(this).find(".modal-body").css("max-height", height);
+});
